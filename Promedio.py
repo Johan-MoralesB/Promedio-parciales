8dbc26b151f8571b35c069c05a544d7efa00c893
Johan Octavio Morales <johanmobe18@gmail.com>
2025-03-13 13:42:44 -0600

    Primera versión.

---
+++ Promedio.py
@@ -0,0 +1,8 @@
+examen1 = float(input("Introduce la calificación del primer examen: "))
+examen2 = float(input("Introduce la calificación del segundo examen: "))
+examen3 = float(input("Introduce la calificación del tercer examen: "))
+
+promedio = (examen1 + examen2 + examen3) / 3
+8
+print(f"El promedio de tus tres exámenes parciales es: {promedio:.2f}")
+input("Presiona Enter para salir...")
